187d2f7eb70c633d5769fb6d26e91f7f3fc464b5
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2023-05-29 14:39:33 +0700

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/QuanBlue/Linux-Setting.git "$HOME/Linux-Setting"
+git clone https://github.com/QuanBlue/Linux-Bootstrap.git "$HOME/Linux-Bootstrap"
 
-cd "$HOME/Linux-Setting"
+cd "$HOME/Linux-Bootstrap"
 
-echo $(pwd)
-echo $(ls -a)
-bash ./prerequisites.sh
+. ./bootstrap.sh
 
-bash ./bootstrap.sh
+. ./prerequisites.sh
