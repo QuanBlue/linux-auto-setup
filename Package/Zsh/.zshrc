e06a50482eafd4aad061153415b7120963badc83
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2023-05-30 16:52:18 +0700

    fix (pakage): zsh

--- Package/Zsh/.zshrc
+++ Package/Zsh/.zshrc
@@ -8,7 +8,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_THEME="spaceship"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -100,5 +100,3 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
-
-eval $(thefuck --alias)
--- Package/Zsh/setup.sh
+++ Package/Zsh/setup.sh
@@ -7,14 +7,15 @@ sudo apt install zsh -y
 chsh -s /bin/zsh 
 
 # Install oh-my-zsh (zsh theme manager)
+rm -rf ~/.oh-my-zsh
 echo -ne 'y\n' | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install power10level theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # install spaceship theme
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt 
+ln -s ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
 
 # Install font
 sudo cp -r "./font/MesloLGS" /usr/share/fonts/truetype/
@@ -36,5 +37,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 sudo apt install python3-dev python3-pip python3-setuptools
 pip3 install thefuck --user
 eval $(thefuck --alias fuck) # set alias
+source ~/.zshrc
 
 
