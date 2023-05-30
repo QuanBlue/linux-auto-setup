183b458f1458b74ae1341997ef019bf1d3d903b0
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2023-05-30 09:18:45 +0700

    update (package): docker

--- Package/Docker/setup.sh
+++ Package/Docker/setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Docker
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+sudo apt update -y
 apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl enable docker
+sudo apt install docker-ce -y
+sudo systemctl enable docker 
 sudo usermod -aG docker $USER
 
 # Docker compose
-sudo apt install docker-compose
+sudo apt install docker-compose -y
