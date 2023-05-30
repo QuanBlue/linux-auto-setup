e6d5dd230d9e4cece4675210294374b7cc9ccb8c
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2023-05-30 09:07:47 +0700

    install Vagrant plugins

--- Package/Vagrant/setup.sh
+++ Package/Vagrant/setup.sh
@@ -2,3 +2,7 @@
 
 wget https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
 sudo apt install ./vagrant_2.2.19_x86_64.deb -y
+
+
+# install vagrant plugin
+vagrant plugin install vagrant-disksize
