05fb7c6dbd952160a8017636227f48013747628d
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2023-06-04 18:23:31 +0700

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -2,9 +2,13 @@ info() {
     tput bold;tput setaf "6";tput setab "7";echo "$1";tput sgr0;tput el;
 }
 
+# Enter Super user
+info "[User] Super user"
+sudo su
+
 # update system
 info "[Update] System"
-sudo apt update && sudo apt upgrade && sudo apt dist-upgrade -y
+sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y
 
 # fix wrong time due dual boot
 info "[Fix] Wrong time due dual boot"
