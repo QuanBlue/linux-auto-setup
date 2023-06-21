8bfcd197a07861436a13d1218122e52be7d15c8a
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2023-06-21 13:14:34 +0700

    build(git): add customize config file - Better-commits

--- Package/Git/setup.sh
+++ Package/Git/setup.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 info() {
-    tput bold;tput setaf "6";tput setab "7";echo "$1";tput sgr0;tput el;
+    tput bold
+    tput setaf "6"
+    tput setab "7"
+    echo "$1"
+    tput sgr0
+    tput el
 }
 
 # We have cd to directory to make sure find only searches in this directory
-dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dotfiles_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$dotfiles_dir"
 
 # Install Git
@@ -18,8 +23,10 @@ cp .gitconfig ~/.gitconfig
 sudo git config --global credential.helper store
 
 info "[Install] Git extensions"
+
 # Git extension
 # better-commits: https://github.com/Everduin94/better-commits
 # Usage: better-commits
 info "[Install] Git extension: better-commits"
 sudo npm install -g better-commits
+cp .better-commits.json ~/.better-commits.json
